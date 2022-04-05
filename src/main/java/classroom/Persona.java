518f66533e73c98cb7c7dea228c08259fd8aebb9
Kevin1875 <94397214+Kevin1875@users.noreply.github.com>
2022-04-05 09:26:21 -0500

    Add files via upload

---
+++ src/main/java/classroom/Persona.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classroom;
+
+/**
+ *
+ * @author usuario
+ */
+public class Persona {
+    
+    long cedula = 1;
+    String nombre;
+    static int totalPersonas;
+    
+    static {
+        totalPersonas = 0;
+        
+    }
+
+    public Persona(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+    
+    public Persona(String nombre, long cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+
+    public Persona(long cedula) {
+        this.cedula = cedula;
+        this.nombre = "";
+        totalPersonas++;
+    }
+
+    public Persona(String nombre) {
+        this.nombre = "";
+        totalPersonas++;
+    }
+    
+    public long getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
